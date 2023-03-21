be900295bc65fe79b9f14f35b78a021e621cdce4
natalia-andrea <103537513+natalia-andrea@users.noreply.github.com>
2023-03-20 22:05:00 -0300

    Update ejercicio.py

--- ejercicio.py
+++ ejercicio.py
@@ -1,5 +1,6 @@
-class Cuenta():
+#Ejercicio 7
 
+class Cuenta():
     def __init__(self, titular="", cantidad=0):
         self.__titular = titular
         self.__cantidad = cantidad
@@ -29,3 +30,36 @@ class Cuenta():
 
     def retirar(self, cantidad):
         self.__cantidad = self.__cantidad - cantidad    
+  
+
+
+#Ejercicio 8
+
+class CuentaJoven(Cuenta):
+    def __init__(self, titular, cantidad, bonificacion):
+        Cuenta.__init__(self, titular, cantidad)
+        self.__bonificacion = bonificacion
+
+    #getter
+    @property
+    def bonificacion(self):
+        return self.__bonificacion
+
+    #setter
+    @bonificacion.setter
+    def bonificacion(self, bonificacion):
+        self.__bonificacion = bonificacion
+
+
+    def es_titular_valido(self):
+        return self.titular.edad >= 18 and self.titular.edad < 25
+
+
+    def retirar(self, cantidad):
+        if self.es_titular_valido():
+            super().retirar(cantidad)
+
+    def mostrar(self):
+        return f"Cuenta Joven {self.bonificacion}"
+ 
+
